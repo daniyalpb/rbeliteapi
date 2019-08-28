00389f034a185b78171836ace2e13678671e4b1d
vikasnerkar <32194410+vikasnerkar@users.noreply.github.com>
2019-08-28 17:55:39 +0530

    new pull

--- resources/views/dashboard/Add_employee.blade.php
+++ resources/views/dashboard/Add_employee.blade.php
@@ -1,11 +1,15 @@
 @extends('include-new.master')
 @section('content')
+@if(Session::has('msg'))
+<div class="alert alert-success alert-dismissible">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<p class="alert alert-success">{{ Session::get('msg') }}</p>
+</div>
+@endif
 
   <div class="container-fluid white-bg col-md-12">
    <div class="col-md-12"><h3 class="mrg-btm"> Add New Employee</h3></div> <br><br>
    <div class="divbro col-md-12">
-    <input type="hidden" name="sfbaid" id="sfbaid" value=""/>
-
 
 <form id="demo_form" name="demo_form" method="post" action="{{url('add-new-emp_elite')}}"> 
  {{csrf_field()}} 
@@ -36,7 +40,7 @@
       <label>Name:</label>
     </div>
     <div class="col-md-7">
-      <input type="text" class="text-primary form-control" name="txtname" id="txtname">
+      <input type="text" class="text-primary form-control" name="txtname" id="txtname" required="">
     </div>
   </div>
 
@@ -45,7 +49,7 @@
       <label>Email Id:</label>
     </div>
     <div class="col-md-7">
-      <input type="text" class="text-primary form-control" name="txtemail" id="txtemail">
+      <input type="text" class="text-primary form-control" name="txtemail" id="txtemail" required="yes" onblur="validateEmail(this);" >
     </div>
   </div>
 
@@ -64,10 +68,10 @@
       <label>Profile:</label>
     </div>
     <div class="col-md-7">
-      <select  name="txtprofile" id="txtprofile" class="text-primary form-control">
+      <select  name="txtprofile" id="txtprofile" class="text-primary form-control" required="yes">
         <option value="">--Select Employee Profile--</option>
         @foreach($profileadd as $val)
-        <option value="{{$val->Profile}}">{{$val->Profile}}</option>
+        <option value="{{$val->id}}">{{$val->Profile}}</option>
         @endforeach
       </select>
     </div>
@@ -136,45 +140,18 @@
   });
 
 </script>
+<script type="text/javascript">
+  function validateEmail(emailField){
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        
+        if (reg.test(emailField.value) == false) 
+        {
+            alert('Invalid Email Address');
+            return false;
+        }
 
-<!-- 
-  <script type="text/javascript">
-        $(document).ready(function(){
-         $("#sub").click(function(){
-               if( $("#search_userid").val()==''){
-
-                  return  false;
-             }
-
-               $.ajax({
-                url: "{{url('new_emp_add')}}",
-                type: 'get',
-                data:{'id':$("#search_userid").val()},
-
-                success: function(data){
-               console.log(data);
-               //alert(newdate);
-                if (data.length<=0) 
-                {
-                  alert("no data found");
-                  // window.location.replace("fbamaster-edit"); 
-                 }else{            
-             $('#txtname').val(data[0].name);
-             $('#txtemail').val(data[0].email);   
-  
-                    }
-
-                }                                
-            });
-        });
-
-    $("#search_userid").keydown(function(){ 
-                      $('#txtname').val('');
-                      $('#txtemail').val(''); 
-                      
-    });
-    });
-          
-  </script> -->
+        return true;
+}
+</script>
 
   @endsection
--- resources/views/dashboard/elite_emp_details.blade.php
+++ resources/views/dashboard/elite_emp_details.blade.php
@@ -1,12 +1,20 @@
 @extends('include-new.master')
 @section('content')
 
-
+@if(Session::has('msg'))
+<div class="alert alert-success alert-dismissible">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<p class="alert alert-success">{{ Session::get('msg') }}</p>
+</div>
+@endif
 <div class="row">
   <div class="col-md-12 grid-margin">
     <div class="card">
             <div class="card-body">
               <h4 class="card-title">Employee Details</h4>
+      
+              <a href="{{url('add-employee')}}" class="btn btn-success adempbtn" id="adempbtn" name="adempbtn">Add New Employee</a>
+
              <!-- <p><a href="#" class="btn btn-primary" data-toggle="modal" data-target="#Agent-Modal"><span class="glyphicon glyphicon-plus"></span> &nbsp; Agent</a></p> -->
             <div class="overflow-scroll">
           <div class="table-responsive">
@@ -30,8 +38,7 @@
           <td>{{$val->official_emailid}}</td>
           <td>{{$val->Profile}}</td>
           <td>{{$val->Status}}</td>
-          <td><a href="update_employee/{{$val->user_id}}" id="btnupdte" class="qry-btn"  value="{{$val->user_id}}" name="btnupdte" class="btn btn-default">Update</a></td>
-
+          <td><a href="update_employee/{{$val->user_id}}" id="btnupdte" class="btn btn-success btnupdte"  value="{{$val->user_id}}" name="btnupdte" class="btn btn-default">Update</a></td>
           </tr>
 		@endforeach
 	</tbody>
@@ -44,4 +51,12 @@
 </div>
 </div>
 
+<script type="text/javascript">
+	$(document).ready( function(){
+    $('#emp_details_tbl').DataTable({
+      "ordering": false
+    });
+
+});
+</script>
   @endsection
